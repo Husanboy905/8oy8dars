c1ae20e50d2de861d83f194fafc09b18c404525b
husanboy <xad78073@gmail.com>
2025-04-17 08:40:31 +0300

    f

--- school/core/urls.py
+++ school/core/urls.py
@@ -2,6 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TeacherViewSet, ClassViewSet, StudentViewSet
 
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
 router = DefaultRouter()
 router.register(r'teachers', TeacherViewSet)
 router.register(r'classes', ClassViewSet)
@@ -9,4 +15,22 @@ router.register(r'students', StudentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+]
+
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="School API",
+        default_version='v1',
+        description="Teacher, Class, Student API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
