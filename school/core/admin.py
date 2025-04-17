c725a27e707f690a0bd5a1ecf8bbee186074adfc
husanboy <xad78073@gmail.com>
2025-04-17 07:47:01 +0300

    f

---
+++ school/core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Teacher, Class, Student
+
+admin.site.register(Teacher)
+admin.site.register(Class)
+admin.site.register(Student)
---
+++ school/core/views.py
@@ -0,0 +1,15 @@
+from rest_framework import viewsets
+from .models import Teacher, Class, Student
+from .serializers import TeacherSerializer, ClassSerializer, StudentSerializer
+
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class ClassViewSet(viewsets.ModelViewSet):
+    queryset = Class.objects.all()
+    serializer_class = ClassSerializer
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
